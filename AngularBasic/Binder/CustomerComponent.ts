bebbd4b2a43cc5dea37d94ef1d46cde904f416ef
krthn <krthn25@gmail.com>
2017-12-17 17:18:38 +0530

    basic angular 2
    commit for reuse

---
+++ AngularBasic/Binder/CustomerComponent.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from "@angular/core"
+import {Customer} from "../Model/Customer"
+
+@Component({
+    selector: "customer-ui",
+    templateUrl: "Customer.html"
+})
+export class CustomerComponent {
+    // binding logic
+    customerobj: Customer = new Customer();
+}
---
+++ AngularBasic/Binder/CustomerModuleLibrary.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CustomerComponent }   from './CustomerComponent';
+import {FormsModule} from "@angular/forms"
+
+@NgModule({
+    imports: [BrowserModule, FormsModule],
+    declarations: [CustomerComponent],
+    bootstrap: [CustomerComponent]
+})
+export class CustomerModuleLibrary { }
